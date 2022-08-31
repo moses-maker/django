ca4357745cdb9a8d9b29d4f0810371e124deb44c
moses-maker <mosesomoto@gmail.com>
2022-08-31 12:35:33 +0300

    created templates

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    search_term = request.GET.get('searchMovie')
+    if search_term:
+        movies = Movie.objects.filter(title__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'name': 'Moses', 'search_term': search_term, 'movies': movies})
+
+
+def about(request):
+
+    return HttpResponse("<h1>Welcome to About page</h1>")
+
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
--- moviereviews/moviereviews/urls.py
+++ moviereviews/moviereviews/urls.py
@@ -1,9 +1,15 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from movie import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movie.urls')),
-    path('', include('news.urls')),
+    path('news/', include('news.urls')),
+    path('about/', views.about, name="about"),
+    path('signup/', views.signup, name="signup"),
+
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
