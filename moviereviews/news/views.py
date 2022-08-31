bf72b8df511a50461623c6d21f9edc1198c854c9
moses-maker <mosesomoto@gmail.com>
2022-08-31 15:18:59 +0300

    created templates

--- moviereviews/news/views.py
+++ moviereviews/news/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 # Create your views here.
 
 
 def news(request):
-    return HttpResponse("<h1>Hello world</h1>")
+    newss = News.objects.all().order_by('-date')
+    return render(request, 'news.html', {'newss': newss})
